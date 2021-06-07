efef6af99075cf6b78913fe012d500d73ac626bc
Alison Miles <alison_miles@outlook.com>
2021-06-07 17:03:03 +0100

    setup react native

---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View, StatusBar } from 'react-native';
+import styles from '../style-sheet';
+
+function WelcomeScreen(props) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.welcomeContainer}>
+        <Text style={styles.welcomeStyle}>Welcome to the Golf App!!</Text>
+      </View>
+      <StatusBar style='auto' />
+    </View>
+  );
+}
+
+export default WelcomeScreen;
---
+++ style-sheet.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  welcomeContainer: {
+    position:'absolute',
+    top: 20
+  },
+  welcomeStyle: {
+    color: 'green',
+    fontSize: 30,
+  },
+});
+
+export default styles;
