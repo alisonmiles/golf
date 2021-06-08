61110e91f5cf2498944c67b89bb907807b864c5e
Alison Miles <alison_miles@outlook.com>
2021-06-08 16:24:20 +0100

    add log in fields to welcome page

--- screens/WelcomeScreen.jsx
+++ screens/WelcomeScreen.jsx
@@ -1,17 +1,40 @@
-import React from 'react';
-import { Text, View, StatusBar } from 'react-native';
+import React, { useState } from 'react';
+import {
+  Text,
+  View,
+  StatusBar,
+  ImageBackground,
+  TextInput,
+} from 'react-native';
 import styles from '../style-sheet';
 
 function WelcomeScreen() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <View style={styles.container}>
-      <View style={styles.welcomeContainer}>
-        <Text style={styles.welcomeStyle}>
-          Hello and welcome to the Golf App!!
-        </Text>
+    <ImageBackground
+      source={require('../assets/golf-background.jpeg')}
+      style={styles.background}
+    >
+      <View style={styles.welcomeHeader}>
+        <Text style={styles.welcomeText}>Golf App</Text>
+      </View>
+      <View style={styles.welcomeInput}>
+        <TextInput
+          placeholder='Email Address'
+          onChangeText={(email) => setEmail(email)}
+        ></TextInput>
+      </View>
+      <View style={styles.welcomeInput}>
+        <TextInput
+          placeholder='Password'
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+        ></TextInput>
       </View>
       <StatusBar style='auto' />
-    </View>
+    </ImageBackground>
   );
 }
 
