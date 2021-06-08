9648389b876df7c8ad844f93abcc9252dcd9ebc7
Alison Miles <alison_miles@outlook.com>
2021-06-08 12:49:45 +0100

    set up a new repo

--- screens/WelcomeScreen.jsx
+++ screens/WelcomeScreen.jsx
@@ -3,7 +3,7 @@ import { Text, View, StatusBar } from 'react-native';
 import styles from '../style-sheet';
 
 function WelcomeScreen() {
-  console.log('hi!')
+  console.log('hello!')
   return (
     <View style={styles.container}>
       <View style={styles.welcomeContainer}>
