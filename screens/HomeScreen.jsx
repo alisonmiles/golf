261344cde02674989f7b472580934c99244bbb18
Alison Miles <alison_miles@outlook.com>
2021-06-08 16:26:28 +0100

    add a basic home screen

---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from '../style-sheet';
+import { View } from 'react-native';
+
+function HomeScreen() {
+  return <View>Home Page/Feed/Leaderboard</View>;
+}
+
+export default HomeScreen;
