f46432b158d40f653b1a7d986c30ca78dece6a04
Joe Grove <joe.grove63@gmail.com>
2021-06-09 22:39:11 +0100

    implemented log-in and sign-up buttons

--- screens/WelcomeScreen.jsx
+++ screens/WelcomeScreen.jsx
@@ -5,6 +5,7 @@ import {
   StatusBar,
   ImageBackground,
   TextInput,
+  TouchableOpacity,
 } from 'react-native';
 import styles from '../style-sheet';
 import firebase from '../src/firebase/config.js';
@@ -13,28 +14,34 @@ function WelcomeScreen() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  firebase
-    .auth()
-    .createUserWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-    })
-    .catch((err) => {
-      const errorCode = err.code;
-      const errorMessage = err.message;
-      console.log(errorCode, errorMessage);
-    });
+  const signUp = (email, password) => {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+        const user = userCredential.user;
+      })
+      .catch((err) => {
+        const errorCode = err.code;
+        const errorMessage = err.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
 
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-    })
-    .catch((err) => {
-      const errorCode = err.code;
-      const errorMessage = err.message;
-    });
+  const logIn = (email, password) => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+        const user = userCredential.user;
+      })
+      .catch((err) => {
+        const errorCode = err.code;
+        const errorMessage = err.message;
+      });
+  };
 
   const user = firebase.auth().currentUser;
 
@@ -48,19 +55,35 @@ function WelcomeScreen() {
       </View>
       <View style={styles.welcomeInput}>
         <TextInput
-          placeholder='Email Address'
+          placeholder="Email Address"
           onChangeText={(email) => setEmail(email)}
         ></TextInput>
       </View>
       <View style={styles.welcomeInput}>
         <TextInput
-          placeholder='Password'
+          placeholder="Password"
           secureTextEntry={true}
           onChangeText={(password) => setPassword(password)}
         ></TextInput>
       </View>
+      <TouchableOpacity
+        style={styles.logInButton}
+        onPress={() => {
+          logIn(email, password);
+        }}
+      >
+        Log in
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.signUpButton}
+        onPress={() => {
+          signUp(email, password);
+        }}
+      >
+        Sign up
+      </TouchableOpacity>
       <View>{user ? <Text>Hello</Text> : <Text>NO USER</Text>}</View>
-      <StatusBar style='auto' />
+      <StatusBar style="auto" />
     </ImageBackground>
   );
 }
