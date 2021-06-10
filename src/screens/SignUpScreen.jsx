73d26faa73043e14f688240d8e131f0865b4e961
shekiel <shekielmorris1@gmail.com>
2021-06-10 15:20:42 +0100

    implement functionality on signup button

--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -1,34 +1,46 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useContext } from 'react';
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
 import FormButton from '../components/FormButton';
 import FormInput from '../components/FormInput';
-export default function SignupScreen() {
+import { AuthContext } from '../navigation/AuthProvider';
+
+export default function SignupScreen({ navigation }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { register } = useContext(AuthContext);
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Create an account</Text>
-      <FormInput
-        value={email}
-        placeholderText="Email"
-        onChangeText={(userEmail) => setEmail(userEmail)}
-        autoCapitalize="none"
-        keyboardType="email-address"
-        autoCorrect={false}
-      />
-      <FormInput
-        value={password}
-        placeholderText="Password"
-        onChangeText={(userPassword) => setPassword(userPassword)}
-        secureTextEntry={true}
-      />
-      <FormButton buttonTitle="Signup" onPress={() => alert('sign button')} />
-    </View>
+    <ImageBackground
+      source={require('../../assets/golfBackground.jpg')}
+      style={styles.container}
+    >
+      <View style={styles.container}>
+        <Text style={styles.text}>Create an account</Text>
+        <FormInput
+          value={email}
+          placeholderText="Email"
+          onChangeText={(userEmail) => setEmail(userEmail)}
+          autoCapitalize="none"
+          keyboardType="email-address"
+          autoCorrect={false}
+        />
+        <FormInput
+          value={password}
+          placeholderText="Password"
+          onChangeText={(userPassword) => setPassword(userPassword)}
+          secureTextEntry={true}
+        />
+        <FormButton
+          buttonTitle="Signup"
+          onPress={() => {
+            register(email, password);
+          }}
+        />
+      </View>
+    </ImageBackground>
   );
 }
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#f5f5f5',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
@@ -36,5 +48,8 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 24,
     marginBottom: 10,
+  },
+  inputField: {
+    backgroundColor: '#FFF',
   },
 });
