51ea3c53db2946442c495154f16629e9aa5ff2be
Joe Grove <joe.grove63@gmail.com>
2021-06-10 12:16:07 +0100

    HomeScreen set up

--- screens/HomeScreen.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from '../style-sheet';
-import { View } from 'react-native';
-
-function HomeScreen() {
-  return <View>Home Page/Feed/Leaderboard</View>;
-}
-
-export default HomeScreen;
