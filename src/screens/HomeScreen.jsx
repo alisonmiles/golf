adef130582015c92934793d37c265dc90ba73c3a
shekiel <shekielmorris1@gmail.com>
2021-06-10 15:18:57 +0100

    add user with logout functionality

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import FormButton from '../components/FormButton';
+import { AuthContext } from '../navigation/AuthProvider';
 export default function HomeScreen() {
+  const { user, logout } = useContext(AuthContext);
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
-      <FormButton buttonTitle="Logout" />
+      <Text style={styles.text}>Welcome {user.uid}</Text>
+      <FormButton buttonTitle="Logout" onPress={() => logout()} />
     </View>
   );
 }
