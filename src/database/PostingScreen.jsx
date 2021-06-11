8bbd39deb8e83e6b2db86db636786a6a5d0816c8
Johann Hannerth <hannerth94@gmail.com>
2021-06-11 15:43:30 +0100

    add code to postingScreen

---
+++ src/database/PostingScreen.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from '../../style-sheet';
+import { firebase, db } from '../firebase/config.js';
+import { Button } from 'react-native';
+function PostingScreen() {
+  function pressButton() {
+    db.collection('users')
+      .doc('mr heworth')
+      .collection('posts')
+      .add({
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        coursename: 'Im a cat',
+        scores: 70,
+        username: 'mr heworth',
+      })
+      .then((documentReference) => {
+        console.log(documentReference);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  return (
+    <Button
+      onPress={() => {
+        pressButton();
+      }}
+      title="POST"
+    ></Button>
+  );
+}
+export default PostingScreen;
