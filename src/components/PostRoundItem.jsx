510154c76493ec551f8ad07f7af5bf29689162e0
shekiel <shekielmorris1@gmail.com>
2021-06-14 16:23:58 +0100

    manipulate first and last slide

--- src/components/PostRoundItem.jsx
+++ src/components/PostRoundItem.jsx
@@ -5,37 +5,45 @@ import {
   View,
   useWindowDimensions,
   TextInput,
+  Button,
 } from 'react-native';
 
 export default function PostRoundItem({ item, setScore, score }) {
   const { width } = useWindowDimensions();
   const [number, onChangeNumber] = useState(0);
-  console.log(score);
 
+  console.log(item.id);
   return (
     <View style={[styles.container, { width }]}>
       <View>
         <Text>{item.title}</Text>
-        <TextInput
-          style={styles.input}
-          onChangeText={onChangeNumber}
-          value={number}
-          onSubmitEditing={() => {
-            setScore((currScore) => {
-              // console.log(typeof number);
-              return currScore + Number(number);
-              //item.id
-              console.log('currScore is', currScore);
-              console.log('number is', number);
-            });
-          }}
-          // console.log(number);
+        {item.id !== '1' ? (
+          <View>
+            <TextInput
+              style={styles.input}
+              onChangeText={onChangeNumber}
+              value={number}
+              onSubmitEditing={() => {
+                setScore((currScore) => {
+                  // console.log(typeof number);
+                  return currScore + Number(number);
+                  //item.id
+                  console.log('currScore is', currScore);
+                  console.log('number is', number);
+                });
+              }}
+              // console.log(number);
 
-          placeholder="Enter your score"
-          keyboardType="numeric"
-          textAlign="center"
-        ></TextInput>
-        <Text>Total {score}</Text>
+              placeholder="Enter your score"
+              keyboardType="numeric"
+              textAlign="center"
+            ></TextInput>
+
+            <Text>Total: {score}</Text>
+          </View>
+        ) : null}
+
+        {item.id === '19' ? <Button title="Post My Round"></Button> : null}
       </View>
     </View>
   );
@@ -48,7 +56,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   input: {
-    height: 100,
+    height: 60,
+    width: 140,
     margin: 12,
     borderWidth: 1,
   },
