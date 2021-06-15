b4af810cea2bfbdc8135a64569c018700b5f8d77
Joe Grove <joe.grove63@gmail.com>
2021-06-15 09:30:33 +0100

    add over/under score

--- src/components/PostRoundItem.jsx
+++ src/components/PostRoundItem.jsx
@@ -7,43 +7,83 @@ import {
   TextInput,
   Button,
 } from 'react-native';
+import firebase from 'firebase/app';
 
-export default function PostRoundItem({ item, setScore, score }) {
+const { db } = firebase;
+
+export default function PostRoundItem({
+  item,
+  setScore,
+  score,
+  parScore,
+  setParScore,
+}) {
   const { width } = useWindowDimensions();
   const [number, onChangeNumber] = useState(0);
 
-  console.log(item.id);
+  const sendToDb = () => {
+    db.collection('posts')
+      .add({
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(), //unchanged
+        coursename: 'Im a cat', // will be hardcoded
+        score: score, // gotten from state
+        overUnderPar: 2, // ditto
+        uid: user.uid, // gotten from user context
+      })
+      .then((documentReference) => {
+        // console.log(documentReference);
+      })
+      .catch((err) => {
+        // console.log(err);
+      });
+  };
+
   return (
     <View style={[styles.container, { width }]}>
       <View>
         <Text>{item.title}</Text>
+        {item.id === '1' ? (
+          <View>
+            <Text>{item.option1.courseName}</Text>
+            <Text>{item.option1.location}</Text>
+            <Text>Par {item.option1.coursePar}</Text>
+          </View>
+        ) : null}
         {item.id !== '1' ? (
           <View>
+            <Text>PAR {item.par}</Text>
+            <Text>{item.yards} YARDS</Text>
             <TextInput
               style={styles.input}
               onChangeText={onChangeNumber}
               value={number}
               onSubmitEditing={() => {
                 setScore((currScore) => {
-                  // console.log(typeof number);
                   return currScore + Number(number);
                   //item.id
-                  console.log('currScore is', currScore);
-                  console.log('number is', number);
+                });
+                setParScore((currPar) => {
+                  return currPar + item.par;
                 });
               }}
-              // console.log(number);
-
               placeholder="Enter your score"
               keyboardType="numeric"
               textAlign="center"
             ></TextInput>
 
             <Text>Total: {score}</Text>
+            <Text> Over/Under {score - parScore}</Text>
           </View>
         ) : null}
 
-        {item.id === '19' ? <Button title="Post My Round"></Button> : null}
+        {item.id === '19' ? (
+          <Button
+            title="Post My Round"
+            onPress={() => {
+              sendToDb();
+            }}
+          ></Button>
+        ) : null}
       </View>
     </View>
   );
